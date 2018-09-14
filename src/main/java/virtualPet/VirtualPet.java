c00039d80d65be1d72bbfab8ca1f76813361acba
WaynePL <wayneplivingston@gmail.com>
2018-09-14 10:49:40 -0400

    Adding some methods to the Virtual Pet

--- src/main/java/virtualPet/VirtualPet.java
+++ src/main/java/virtualPet/VirtualPet.java
@@ -2,4 +2,28 @@ package virtualPet;
 
 public class VirtualPet {
 
+	boolean isAlive = true;
+
+	// This is the constructor.
+	public VirtualPet() {
+		// The pet is now alive. This means the loop should start
+		status();
+	}
+
+	public void status() {
+
+		while (isAlive) {
+			System.out.println("Animal noise");
+
+		}
+	}
+
+	// this is the main display
+	public void display() {
+
+	}
+
+	public void tick() {
+		System.out.println("This is a tick. things are happening and progressing.");
+	}
 }
